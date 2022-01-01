d4b1c84e2267999180ec079ab5dca20c1ffa07f5
Gui774ume <guillaume.fournier@datadoghq.com>
2022-01-01 16:13:40 +0100

    fetch manager pid using host proc fs instead of os.Getpid()

--- utils.go
+++ utils.go
@@ -614,3 +614,40 @@ func getTCMsgInfo(prio, protocol uint16) uint32 {
 	binary.LittleEndian.PutUint16(res[2:4], protocol)
 	return binary.BigEndian.Uint32(res)
 }
+
+//GetEnv retrieves the environment variable key. If it does not exist it returns the default.
+func GetEnv(key string, dfault string, combineWith ...string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		value = dfault
+	}
+
+	switch len(combineWith) {
+	case 0:
+		return value
+	case 1:
+		return filepath.Join(value, combineWith[0])
+	default:
+		all := make([]string, len(combineWith)+1)
+		all[0] = value
+		copy(all[1:], combineWith)
+		return filepath.Join(all...)
+	}
+}
+
+// HostProc returns joins the provided path with the host /proc directory
+func HostProc(combineWith ...string) string {
+	return GetEnv("HOST_PROC", "/proc", combineWith...)
+}
+
+// Getpid returns the current process ID in the host namespace if $HOST_PROC is defined, the pid in the current namespace
+// otherwise
+func Getpid() int {
+	p, err := os.Readlink(HostProc("/self"))
+	if err == nil {
+		if pid, err := strconv.ParseInt(p, 10, 32); err == nil {
+			return int(pid)
+		}
+	}
+	return os.Getpid()
+}
