53e1e5af2574c6d9b0b54ea517a45cee4d6fec52
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-09-06 13:08:10 -0700

    move syscalls

--- syscalls.go
+++ syscalls.go
@@ -1,2 +1,130 @@
 package manager
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
+// cache of the syscall prefix depending on kernel version
+var syscallPrefix string
+
+// GetSyscallFnName - Returns the kernel function of the provided syscall, after reading /proc/kallsyms to retrieve
+// the list of symbols of the current kernel.
+func GetSyscallFnName(name string) (string, error) {
+	return GetSyscallFnNameWithSymFile(name, defaultSymFile)
+}
+
+// GetSyscallFnNameWithSymFile - Returns the kernel function of the provided syscall, after reading symFile to retrieve
+// the list of symbols of the current kernel.
+func GetSyscallFnNameWithSymFile(name string, symFile string) (string, error) {
+	if symFile == "" {
+		symFile = defaultSymFile
+	}
+	if syscallPrefix == "" {
+		syscallName, err := getSyscallName("open", symFile)
+		if err != nil {
+			return "", err
+		}
+		// copy to avoid memory leak due to go subslice
+		// see: https://go101.org/article/memory-leaking.html
+		var b strings.Builder
+		b.WriteString(syscallName)
+		syscallName = b.String()
+
+		syscallPrefix = strings.TrimSuffix(syscallName, "open")
+	}
+
+	return syscallPrefix + name, nil
+}
+
+const defaultSymFile = "/proc/kallsyms"
+
+// Returns the qualified syscall named by going through '/proc/kallsyms' on the
+// system on which its executed. It allows bpf programs that may have been compiled
+// for older syscall functions to run on newer kernels
+func getSyscallName(name string, symFile string) (string, error) {
+	// Get kernel symbols
+	syms, err := os.Open(symFile)
+	if err != nil {
+		return "", err
+	}
+	defer syms.Close()
+
+	return getSyscallFnNameWithKallsyms(name, syms, "")
+}
+
+func getSyscallFnNameWithKallsyms(name string, kallsymsContent io.Reader, arch string) (string, error) {
+	if arch == "" {
+		switch runtime.GOARCH {
+		case "386":
+			arch = "ia32"
+		case "arm64":
+			arch = "arm64"
+		default:
+			arch = "x64"
+		}
+	}
+
+	// We should search for new syscall function like "__x64__sys_open"
+	// Note the start of word boundary. Should return exactly one string
+	newSyscall := regexp.MustCompile(`\b__` + arch + `_[Ss]y[sS]_` + name + `\b`)
+	// If nothing found, search for old syscall function to be sure
+	oldSyscall := regexp.MustCompile(`\b[Ss]y[sS]_` + name + `\b`)
+	// check for '__' prefixed functions, like '__sys_open'
+	prefixed := regexp.MustCompile(`\b__[Ss]y[sS]_` + name + `\b`)
+
+	// the order of patterns is important
+	// we first want to look for the new syscall format, then the old format, then the prefixed format
+	patterns := []struct {
+		pattern *regexp.Regexp
+		result  string
+	}{
+		{newSyscall, ""},
+		{oldSyscall, ""},
+		{prefixed, ""},
+	}
+
+	scanner := bufio.NewScanner(kallsymsContent)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if !strings.Contains(line, name) {
+			continue
+		}
+
+		for i := range patterns {
+			p := &patterns[i]
+			// if we already found a match for this pattern we continue
+			if p.result != "" {
+				continue
+			}
+
+			if res := p.pattern.FindString(line); res != "" {
+				// fast path for first match on first pattern
+				if i == 0 {
+					return res, nil
+				}
+
+				p.result = res
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	for _, p := range patterns {
+		if p.result != "" {
+			return p.result, nil
+		}
+	}
+
+	return "", fmt.Errorf("could not find a valid syscall name")
+}
--- utils_test.go
+++ utils_test.go
@@ -1,8 +1,6 @@
 package manager
 
 import (
-	"bytes"
-	"fmt"
 	"testing"
 )
 
@@ -31,79 +29,5 @@ func TestGenerateEventName(t *testing.T) {
 	_, err = generateEventName(probeType, funcName, UID, kprobeAttachPID)
 	if err == nil {
 		t.Errorf("Test should failed as event name length is too big for the kernel and free space for function Name is < %d", minFunctionNameLen)
-	}
-}
-
-func TestGetSyscallFnNameWithKallsyms(t *testing.T) {
-	entries := []struct {
-		fnName          string
-		kallsymsContent string
-		expected        string
-	}{
-		{
-			fnName: "open",
-			kallsymsContent: `
-0000000000000000 T do_fchownat
-0000000000000000 T __arm64_sys_fchownat
-0000000000000000 T __arm64_sys_chown
-0000000000000000 T __arm64_sys_lchown
-0000000000000000 T vfs_fchown
-0000000000000000 T ksys_fchown
-0000000000000000 T __arm64_sys_fchown
-0000000000000000 T vfs_open
-0000000000000000 T build_open_how
-0000000000000000 T build_open_flags
-0000000000000000 t do_sys_openat2
-0000000000000000 t __do_sys_openat2
-0000000000000000 T __arm64_sys_openat2
-0000000000000000 T __arm64_sys_creat
-0000000000000000 T __arm64_compat_sys_open
-0000000000000000 T __arm64_compat_sys_openat
-0000000000000000 T __arm64_sys_open
-0000000000000000 T __arm64_sys_openat
-0000000000000000 T file_open_name
-0000000000000000 T do_sys_open
-0000000000000000 T vfs_setpos
-0000000000000000 T generic_file_llseek_size
-0000000000000000 T generic_file_llseek
-0000000000000000 T fixed_size_llseek
-0000000000000000 T no_seek_end_llseek
-0000000000000000 T no_seek_end_llseek_size
-0000000000000000 T noop_llseek
-0000000000000000 T vfs_llseek
-0000000000000000 T default_llseek
-0000000000000000 t arch_local_irq_save
-	`,
-			expected: "__arm64_sys_open",
-		},
-		{
-			fnName: "connect",
-			kallsymsContent: `
-0000000000000000 T __sys_connect
-0000000000000000 T __arm64_sys_connect
-	`,
-			expected: "__arm64_sys_connect",
-		},
-		{
-			fnName: "open",
-			kallsymsContent: `
-0000000000000000 T __SyS_open
-0000000000000000 T __sys_open
-	`,
-			expected: "__SyS_open",
-		},
-	}
-
-	for i, testEntry := range entries {
-		t.Run(fmt.Sprintf("%s_%d", testEntry.fnName, i), func(t *testing.T) {
-			res, err := getSyscallFnNameWithKallsyms(testEntry.fnName, bytes.NewBuffer([]byte(testEntry.kallsymsContent)), "arm64")
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if res != testEntry.expected {
-				t.Errorf("expected %s, got %s", testEntry.expected, res)
-			}
-		})
 	}
 }
