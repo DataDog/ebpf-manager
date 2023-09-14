fa5a7637b327483de23f22cd5bacace4b95b9636
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-09-14 16:13:22 +0200

    add benchmark

--- utils_test.go
+++ utils_test.go
@@ -3,6 +3,7 @@ package manager
 import (
 	"bytes"
 	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -105,5 +106,23 @@ func TestGetSyscallFnNameWithKallsyms(t *testing.T) {
 				t.Errorf("expected %s, got %s", testEntry.expected, res)
 			}
 		})
+	}
+}
+
+func BenchmarkFindFilterFunction(b *testing.B) {
+	var needle string
+	switch runtime.GOARCH {
+	case "arm64":
+		needle = "__arm64_sys_open"
+	default:
+		needle = "__x64_sys_open"
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, err := FindFilterFunction(needle)
+		if err != nil {
+			b.Error(err)
+		}
+		clearAvailableFilterFunctionsCache()
 	}
 }
