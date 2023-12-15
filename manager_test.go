69a9d9e6ede6ff1dfcc294ea9a742593f5f3cd31
Guillermo Julián <guillermo.julian@datadoghq.com>
2023-12-15 13:50:49 -0800

    change signature of DumpMaps to use a io.Writer

--- manager_test.go
+++ manager_test.go
@@ -1,7 +1,9 @@
 package manager
 
 import (
+	"bytes"
 	"errors"
+	"io"
 	"math"
 	"os"
 	"strings"
@@ -10,6 +12,7 @@ import (
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/asm"
 	"github.com/cilium/ebpf/rlimit"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/sys/unix"
 )
 
@@ -151,4 +154,55 @@ func TestManager_getTracefsRegex(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDumpMaps(t *testing.T) {
+	err := rlimit.RemoveMemlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m := &Manager{
+		Probes: []*Probe{
+			{ProbeIdentificationPair: ProbeIdentificationPair{EBPFFuncName: "access_map_one"}},
+		},
+		Maps: []*Map{
+			{Name: "map_one"},
+		},
+	}
+
+	opts := Options{
+		RLimit: &unix.Rlimit{
+			Cur: math.MaxUint64,
+			Max: math.MaxUint64,
+		},
+		ExcludedFunctions: []string{"access_map_two"},
+	}
+
+	f, err := os.Open("testdata/exclude.elf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { _ = f.Close() })
+
+	err = m.InitWithOptions(f, opts)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dumpContents := "mapdump"
+
+	m.DumpHandler = func(_ *Manager, mapName string, currentMap *ebpf.Map, w io.Writer) {
+		io.WriteString(w, dumpContents)
+	}
+
+	var output bytes.Buffer
+	err = m.DumpMaps(&output, "map_one")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, dumpContents, output.String())
 }
